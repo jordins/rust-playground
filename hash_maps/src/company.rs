f2a460aec2d46052257153dcb133e4c3edfee6fd
Jordi Nistal <jordi.nistal@gmail.com>
2019-02-10 20:03:57 +0100

    add test to add_employee function
    No sure if this is the proper way to test a method because this
    seems to be an integration test and because I had to create a
    private method to do the test.

--- hash_maps/src/company.rs
+++ hash_maps/src/company.rs
@@ -37,4 +37,36 @@ impl Company {
             None => println!("Sorry, the department {} is empty", department),
         }
     }
+
+    fn count_persons_from_department(&self, department: &String) -> usize {
+        let persons_from_department = self.departments.get(department);
+        match persons_from_department {
+            Some(names) => names.len(),
+            None => 0,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn add_employee() {
+        let mut company = Company::new();
+        let department = "myDepartment";;
+        _create_and_add_employee_to_department(&mut company, department.to_string());
+
+        let num_of_persons = company.count_persons_from_department(&department.to_string());
+        assert_eq!(num_of_persons, 1);
+
+        _create_and_add_employee_to_department(&mut company, department.to_string());
+        let num_of_persons = company.count_persons_from_department(&department.to_string());
+        assert_eq!(num_of_persons, 2);
+    }
+
+    fn _create_and_add_employee_to_department(company: &mut Company, department_name: String) {
+        let employee = EmployeeInfo::new(String::from("myName"), department_name);
+        company.add_employee(&employee);
+    }
 }
